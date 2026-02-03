5f33b2efa561ce3dbdf4b9d53ab7ffa54e489813
Nguyen Loc <locdedoda@gmail.com>
2026-02-03 23:18:46 +0700

    chore: edit pretter config

--- backend/app/config/index.js
+++ backend/app/config/index.js
@@ -2,12 +2,12 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const config = {
-   app: {
-      port: process.env.PORT || 3000,
-   },
-   db: {
-      uri: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lexilibrary"
-   }
-}
+  app: {
+    port: process.env.PORT || 3000,
+  },
+  db: {
+    uri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/lexilibrary',
+  },
+};
 
 module.exports = config;
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,19 @@
 const app = require('./app');
-const MongoDB = require('./app/utils/mongodb.util')
+const MongoDB = require('./app/utils/mongodb.util');
 const config = require('./app/config');
-
 async function startServer() {
-   try {
-      await MongoDB.connect(config.db.uri);
-      console.log('Connect to the databse successfully!');
+  try {
+    await MongoDB.connect(config.db.uri);
+    console.log('Connect to the databse successfully!');
 
-      const PORT = config.app.port;
-      app.listen(PORT, () => {
-         console.log(`Server is running on port ${PORT}`);
-      })
-   } catch (error) {
-      console.log('Cannot start the server!', error)
-      process.exit();
-   }
+    const PORT = config.app.port;
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log('Cannot start the server!', error);
+    process.exit();
+  }
 }
 
 startServer();
