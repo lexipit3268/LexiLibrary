b63a82f03bedf489ec3d884da6bf0ae9a1292cdd
Nguyen Loc <locdedoda@gmail.com>
2026-02-04 21:30:28 +0700

    package: add nodemon

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to contact book application.' });
+  res.json({ message: 'Welcome to Lexilibrary.' });
 });
 
 module.exports = app;
