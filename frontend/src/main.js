f70292a4d8fa7e02bdfcde2f56726dbb7fc63b47
Nguyen Loc <locdedoda@gmail.com>
2026-02-11 20:04:13 +0700

    package: add Splidejs

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,11 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import VueSplide from '@splidejs/vue-splide'
 import './assets/index.css'
 
 const app = createApp(App)
 app.use(ElementPlus)
+app.use(VueSplide)
 app.use(router)
 app.mount('#app')
