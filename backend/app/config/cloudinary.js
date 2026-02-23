d10fc94b1d6b896054dcc3ebb26eed884f1a87ed
Nguyen Loc <locdedoda@gmail.com>
2026-02-23 19:36:16 +0700

    be-feat: finish cloudinary config

--- backend/app/config/cloudinary.js
+++ backend/app/config/cloudinary.js
@@ -14,7 +14,7 @@ const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
     folder: 'LexiLibrary',
-    allowed_formats: ['jpg', 'png', 'jpeg'],
+    allowed_formats: ['jpg', 'png', 'jpeg', 'webp'],
   },
 });
 const uploadCloud = multer({ storage });
