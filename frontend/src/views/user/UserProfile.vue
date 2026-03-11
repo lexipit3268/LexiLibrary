4cabe00bc9d652701d9e7211031b0d93c81d951a
Nguyen Loc <locdedoda@gmail.com>
2026-03-11 14:06:03 +0700

    fe-feat: enhance UserProfile component to redirect staff users

--- frontend/src/views/user/UserProfile.vue
+++ frontend/src/views/user/UserProfile.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="min-h-120 bg-pattern py-16 px-4">
-    <RouterView></RouterView>
+    <RouterView v-if="authStore.userRole !== 'staff'"></RouterView>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { useAuthStore } from '@/stores/auth'
+import { useRouter } from 'vue-router'
+
+const authStore = useAuthStore()
+const router = useRouter()
+if (authStore.userRole === 'staff') {
+  router.push('/staff/me')
+}
+</script>
